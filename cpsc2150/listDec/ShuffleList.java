373db15c82c9da375bd1416fed6cf1fd7b659537
Camden Spehl <47334268+CamdenSpehl@users.noreply.github.com>
2023-10-31 09:38:50 -0400

    Update ShuffleList.java

--- cpsc2150/listDec/ShuffleList.java
+++ cpsc2150/listDec/ShuffleList.java
@@ -42,32 +42,32 @@ public class ShuffleList<T> implements IShuffleList<T> {
 
     @Override
     public boolean add(T e) {
-        // complete this method
+        return myList.add(e);
     }
 
     @Override
     public T get(int i) {
-        // complete this method
+        return myList.get(i);
     }
 
     @Override
     public void add(int index, T e) {
-        // complete this method
+        myList.add(index, e);
     }
 
     @Override
     public int size() {
-        // complete this method
+        return myList.size();
     }
 
     @Override
     public void clear() {
-        // complete this method
+        myList.clear();
     }
 
     @Override
     public T set(int i, T x) {
-        // complete this method
+        return myList.set(i, x);
     }
 
     @Override
