52e6d80695be8f42a3d53e119607c7336484e6d5
Camden Spehl <47334268+CamdenSpehl@users.noreply.github.com>
2023-10-31 09:52:06 -0400

    fixed random

--- cpsc2150/listDec/IShuffleList.java
+++ cpsc2150/listDec/IShuffleList.java
@@ -1,12 +1,14 @@
 package cpsc2150.listDec;
 
 import java.util.List;
+import java.util.Random;
 
 public interface IShuffleList<T> extends List<T> {
     default void shuffle(int swaps) {
+        Random rand = new Random();
         for (int i = 0; i < swaps; i++) {
-            int j = (int) (Math.random() * size());
-            int k = (int) (Math.random() * size());
+            int j = rand.nextInt(size());
+            int k = rand.nextInt(size());
             swap(j, k);
         }
     }
